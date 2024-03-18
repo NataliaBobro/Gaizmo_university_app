c9460e4ac8726a3fe975f676cc11858783116f82
bodyast <bodyast1996@gmail.com>
2024-03-19 01:00:20 +0200

    render android app

--- android/app/src/main/kotlin/com/example/fimex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.etm
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/ua/ETM/etmcrm/MainActivity.kt
@@ -0,0 +1,5 @@
+package ua.ETM.etmcrm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
